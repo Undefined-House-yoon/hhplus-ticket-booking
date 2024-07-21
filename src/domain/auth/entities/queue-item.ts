08e203796bc5fbb5a0837e1303421e4bdf352380
asd <pa6712@naver.com>
2024-07-22 00:44:32 +0900

    feat : queue-item 기능 구현(대기열)

--- src/domain/auth/entities/queue-item.ts
+++ src/domain/auth/entities/queue-item.ts
@@ -1,6 +1,40 @@
-export interface QueueItem {
-  id: number;
-  userId: number;
-  processed: boolean;
-  createdAt: Date;
+export class QueueItem {
+
+  readonly id: number;
+  readonly userId: number;
+  private _processed: boolean;
+  readonly createdAt?: Date;
+
+  //대기 처리중
+  isProcessed(): boolean {
+    return this._processed;
+  }
+
+  // 대기 끝 입장
+  shouldProcess() {
+    this._processed = true;
+  }
+
+  static create(params: {
+    id: number;
+    userId: number;
+    processed: boolean;
+    createdAt?: Date;
+  }) {
+   return new QueueItem(params);
+  }
+
+  private constructor({ id, userId, processed, createdAt }: {
+    id: number;
+    userId: number;
+    processed: boolean;
+    createdAt?: Date;
+  }) {
+    this.id = id;
+    this.userId = userId;
+    this._processed = createdAt ? processed : false;
+    this.createdAt = createdAt;
+  }
+
+
 }
